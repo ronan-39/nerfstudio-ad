2dfa32c56124ae196453d0c60b26669226f90158
Ronan <67182906+ronan-39@users.noreply.github.com>
2024-07-18 15:32:29 -0400

    updated encoder and dataloader

--- matloc_impl/encoder.py
+++ matloc_impl/encoder.py
@@ -3,8 +3,20 @@ A CNN to encode images to the space of NeRF features
 """
 
 import torch
+import torchvision
 import torchvision.models as models
 from torch import Tensor, nn
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+import torch.nn.functional as F
+import torch.optim
+from torchvision import datasets,transforms
+import numpy as np
+from pathlib import PosixPath, Path
+import os
+import pickle
+import matplotlib.pyplot as plt
+import nerfstudio
+from nerfstudio.utils.colormaps import ColormapOptions
 
 # load resnet 18
 # model = models.resnet18(weights=None)
@@ -16,35 +28,92 @@ from torch import Tensor, nn
 
 class Encoder(nn.Module):
     def __init__(self):
-        super().__init__()
+        super(Encoder, self).__init__()
 
-        self.encoder = nn.Sequential(
-            nn.ConvTranspose2d(3, 16,
-                              kernel_size=3,
-                              stride=2,
-                              padding=1,
-                              output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(16, 32,
-                               kernel_size=3,
-                               stride=2,
-                               padding=1,
-                               output_padding=1),
-            nn.ReLU(),
-            nn.ConvTranspose2d(32, 64,
-                               kernel_size=3,
-                               stride=2,
-                               padding=1,
-                               output_padding=1),
-            nn.Sigmoid()
-        )
+        self.en_conv1 = nn.Conv2d(3, 16, 3, padding=1, stride=1)
+        self.en_conv2 = nn.Conv2d(16, 32, 3, padding=1, stride=1)
+        self.en_conv3 = nn.Conv2d(32, 64, 3, padding=1, stride=1)    
 
     def forward(self, x):
-        return self.encoder(x)
-    
+        x = F.relu(self.en_conv1(x))
+        x = F.relu(self.en_conv2(x))
+        x = self.en_conv3(x)
+        return x
+
+class EncodingDataset(Dataset):
+    def __init__(self, data_dir):
+        super().__init__()
+
+        self.data_dir = data_dir
+        self.len = self.__len__()
+
+    def __len__(self):
+        return len([name for name in os.listdir(self.data_dir.joinpath("features")) if name.endswith(".feature")])
+
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        if idx > self.len:
+            raise Exception("Out of bounds")
+
+        input_filepath = self.data_dir.joinpath(f'rgb/rgb_{idx}.png')
+        output_filepath = self.data_dir.joinpath(f'features/feat_{idx}.feature')
+
+        input = torchvision.io.read_image(str(input_filepath))
+
+        output_file = open(output_filepath, 'rb')
+        output = pickle.load(output_file)
+
+        if output.shape[2] == 64: # its in the wrong order, a remnant of earlier code
+            output = output.permute(2,0,1)
+            
+        return {'input': input, 'desired_output': output}
+            
+def preview_dataset(dataset):
+
+    fig, axes = plt.subplots(nrows=2, ncols=6, sharex=True, sharey=True, figsize=(48,14))
+
+    ins = [dataset.__getitem__(i)['input'].permute(1,2,0).numpy() for i in range(6)]
+    outs = [nerfstudio.utils.colormaps.apply_colormap(dataset.__getitem__(i)['desired_output'].cpu().permute(1,2,0), colormap_options=ColormapOptions()).numpy() for i in range(6)]
+
+    for imgs,row in zip([ins, outs], axes):
+        for img, ax in zip(imgs, row):
+            ax.imshow(np.squeeze(img), cmap='gray')
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+    plt.show()
+
 if __name__ == "__main__":
     print("running encoder.py")
     model = Encoder()
+
+    if torch.cuda.is_available():
+        model.cuda()
+
+    training_data_dir = Path("./outputs/unnamed/feature_image_pairs")
+
+    if not training_data_dir.exists():
+        print("The training data directory does not exist. Exiting.")
+        import sys
+        sys.exit()
+
+    encoder_model_dir = training_data_dir.joinpath("encoder_models")
+    if not Path.exists(encoder_model_dir):
+        Path.mkdir(encoder_model_dir)
+      
+    valid_size = 0.2
+    batch_size = 20
+    num_workers=8
+
+    dataset = EncodingDataset(training_data_dir)
+    
+    # preview_dataset(dataset)
+
+    dataloader = DataLoader(dataset, batch_size=5, sampler=SubsetRandomSampler(list(range(dataset.len))), num_workers=0)
+    
 
 """
 ResNet(
